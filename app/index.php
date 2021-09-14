88d1dae5b0c489b9f5d937d0478841e1455ef257
Aref <haji4ref@gmail.com>
2021-09-14 12:25:48 +0430

    Init commit

---
+++ app/index.php
@@ -0,0 +1,33 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use Mohammadv184\ArCaptcha\ArCaptcha;
+
+    $SITE_KEY = getenv("SITE_KEY"); // replace your site_key
+
+    $SECRET_KEY = getenv("SECRET_KEY"); // replace your secret_key
+
+    $ArCaptcha = new ArCaptcha($SITE_KEY, $SECRET_KEY);
+?>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') { // check form submission
+        if ($ArCaptcha->verify($_POST["arcaptcha-token"])) { // verify that captcha is solved
+            echo "OK!";
+        } else {
+            echo "FAILED!";
+        }
+    } else { // show form
+    ?>
+    <!DOCTYPE html>
+        <head></head>
+        <body>
+            <?php echo $ArCaptcha->getScript() // try to inject script into body ?>
+            <form method="POST">
+                <?php echo $ArCaptcha->getWidget() // try to inject div with arcaptcha class ?>
+                <input type="submit" value="Submit" />
+            </form>
+        </body>
+    </html>
+    <?php
+        }
+    ?>
